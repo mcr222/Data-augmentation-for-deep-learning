0c9422f999d8b111bb41a7c945b6949c6e147ef2
mcr222 <markitus222@hotmail.com>
2017-10-31 18:47:39 +0100

    Add functions headers

--- ANN.py
+++ ANN.py
@@ -1,7 +1,18 @@
 import tensorflow as tf
+import Data_augmentation as da
 
 
-# import tensorflow as tf
-# hello = tf.constant('Hello, TensorFlow!')
-# sess = tf.Session()
-# print(sess.run(hello))
+'''
+Trains an ANN with tensorflow
+    transformations -> list of numbers of the transformed sets of images to be used
+    dataset -> which dataset to train with
+    image_number -> number of images randomly selected from the original dataset to use 
+        for training (rest are for testing) need to save which images are use for training 
+        to do the test with the other images
+    ANN_parameters -> tuple with all parameters for the ANN
+'''
+def trainANN(transformations, dataset, image_number, ANN_parameters):
+    print "training"
+
+    
+    
---
+++ Data_augmentation.py
@@ -0,0 +1,41 @@
+dataset_folder = "data/dataset"
+original_images = "/original"
+transformation_images = "/transformation"
+
+'''
+Applies a data augmentation technique into a dataset
+    transformation -> which # of transformation to apply, if -1 return original folder
+    dataset -> which # of dataset to apply to
+'''
+def getImageFolder(transformation, dataset):
+    if(transformation <0):
+        return dataset_folder+str(dataset)+original_images
+    
+    return dataset_folder+str(dataset)+transformation_images+str(transformation)
+
+
+
+'''
+Applies a data augmentation technique into a dataset
+    transformation -> which # of transformation to apply
+    dataset -> which # of dataset to apply to
+'''
+def dataAugmentation(transformation, dataset):
+  list_transformations[transformation](getImageFolder(-1,dataset),getImageFolder(transformation,dataset)) 
+    
+'''
+Explain transformation
+'''
+def transformation0(input_folder, output_folder):
+    print "applying to " + input_folder + " writing to " + output_folder
+
+'''
+Explain transformation
+'''
+def transformation1(input_folder, output_folder):
+    print "applying to " + input_folder + " writing to " + output_folder
+    
+list_transformations = [transformation0, transformation1]
+
+dataAugmentation(0,0)
+
