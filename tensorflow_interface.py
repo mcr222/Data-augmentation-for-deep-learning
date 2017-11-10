a46f09a41ac4caeee2a2568f7aba04591086baf4
mcr222 <markitus222@hotmail.com>
2017-11-10 02:03:41 +0100

    Heavy refactor of ANN models

---
+++ tensorflow_interface.py
@@ -0,0 +1,43 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+
+
+# dimensions of our images.
+img_width, img_height = 150, 150
+
+train_data_dir = 'dataset1/train'
+validation_data_dir = 'dataset1/validation'
+#TODO: adapt train and validation samples numbers
+nb_train_samples = 100
+nb_validation_samples = 100
+epochs = 10
+batch_size = 16
+
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
+
+datagen = ImageDataGenerator(rescale=1. / 255)
+
+def getTrainGenerator():
+    train_generator = datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode='binary')
+    return train_generator
+    
+def getValidationGenerator():
+    validation_generator = datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode='binary')
+    return validation_generator
+
+    
